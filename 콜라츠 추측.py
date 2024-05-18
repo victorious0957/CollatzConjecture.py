f41fcd53f5eac56683b00b731825dedb805efcf5
victorious0957 <bluev9@gmail.com>
2024-05-18 20:35:39 +0900

    Revert "Add files via upload"
    This reverts commit 7bc0501c010b5252f42d94b9506756052ec997d0.

--- 콜라츠 추측.py
+++ 콜라츠 추측.py
@@ -1,19 +1,8 @@
-min=int(input("시작 숫자 입력\n"))
-max=int(input("종료 숫자 입력, 0이면 무한\n"))
-sx=int(min)
-x=int(sx)
-if min>=max:
-    print("min이 max보다 크거나 같습니다.")
-    exit()
-while max==0 or  sx<=max:
-    x=int(sx)
-    while not x==1:
-        if(x%2==1):
-                x=3*x+1
-        else:
-                x=x/2
-        print(x)
-    print("콜라츠 추측이 이 수에서 성립합니다")
-    sx=sx+1
-sx=sx-1
-print(str(min)+"부터 "+ str(sx)+"까지의 수에서 콜라츠 추측이 성립합니다")
+x=int(input("원하는 숫자 입력"))
+while not x==1:
+    if(x%2==1):
+            x=3*x+1
+    else:
+            x=x/2
+    print(x)
+print("콜라츠 추측이 성립합니다")
