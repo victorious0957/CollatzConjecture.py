2f5ab73f7bafcddf43baf1af403e98b7008c3093
victorious09 <138985654+victorious0957@users.noreply.github.com>
2024-05-18 20:12:44 +0900

    Update 콜라츠 추측.py

--- 콜라츠 추측.py
+++ 콜라츠 추측.py
@@ -1,4 +1,3 @@
-s=0
 x=int(input("원하는 숫자 입력"))
 while not x==1:
     if(x%2==1):
@@ -6,5 +5,4 @@ while not x==1:
     else:
             x=x/2
     print(x)
-if s==0:
-    print("콜라츠 추측이 성립합니다")
+print("콜라츠 추측이 성립합니다")
