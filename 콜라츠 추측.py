36ac22e141e8aeca53570f9e3bccbb5fa10da9d8
victorious09 <138985654+victorious0957@users.noreply.github.com>
2024-05-18 20:06:23 +0900

    ㅇ

---
+++ 콜라츠 추측.py
@@ -0,0 +1,15 @@
+s=0
+x=int(input("원하는 숫자 입력"))
+while not x==1:
+    if x<=295147905179352825856:
+        print("콜라츠 추측이 성립합니다")
+        s=1
+        break
+    else:
+        if(x%2==1):
+            x=3*x+1
+        else:
+            x=x/2
+    print(x)
+if s==0:
+    print("콜라츠 추측이 성립합니다")
